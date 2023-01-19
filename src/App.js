e62fd75b83ed81de6305f7b368ed50120c97bac0
TylerS2000 <tylerschulten@gmail.com>
2023-01-18 19:13:02 -0500

    exercises 1-4 completed

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ const App = () => {
   const[title, setTitle] = useState('')
   const[author, setAuthor] = useState('')
   const[url, setUrl] = useState('')
+  const[message, setMessage] = useState(null)
 console.log(user)
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -23,7 +24,7 @@ console.log(username,password)
 
     <div>
       {user === null ? <Login password={password} username={username} setUser={setUser} setUsername={setUsername} setPassword={setPassword} /> :
-       <div>Logged in as {user.username} <button onClick={()=>{setUser(null)}}>Logout</button> <AddBlog title={title} setTitle={setTitle} author={author} setAuthor={setAuthor} url={url} setUrl={setUrl}/> </div>}
+       <div>{message}Logged in as {user.username} <button onClick={()=>{setUser(null)}}>Logout</button> <AddBlog setMessage = {setMessage}title={title} setTitle={setTitle} author={author} setAuthor={setAuthor} url={url} setUrl={setUrl}/> </div>}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -7,8 +7,16 @@ export default function AddBlog(props) {
         try {
             const response = await axios.post("http://localhost:3003/api/blogs", object,config);
             console.log(response.data);
+            props.setMessage(`Added ${response.data.title} by ${response.data.author}`)
+            setTimeout(() => {
+                props.setMessage(null)
+            }, 5000);
         } catch (error) {
             console.log(error);
+            props.setMessage(`Unsuccessful addition of blog`)
+            setTimeout(() => {
+                props.setMessage(null)
+            }, 5000);
         }
     };
       console.log(config)
@@ -21,7 +29,7 @@ export default function AddBlog(props) {
             <input type="text" name="author" value={props.author} onChange={(event)=>props.setAuthor(event.target.value)} />
             <label>Url</label>
             <input type="text" name="url" value={props.url} onChange={(event)=>props.setUrl(event.target.value)}/>
-            <button type="submit" onClick={async ()=>createBlog({url:props.url,author:props.author,title:props.title})}>Add</button>
+            <button type="submit" onClick={async (event)=> { event.preventDefault(); createBlog({url:props.url,author:props.author,title:props.title})}}>Add</button>
         </form>
         </div>
     );
